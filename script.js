14f87dbba6c84ac0db87275f2a75a766cf474c4f
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-13 16:08:03 +0200

    Working With Classes

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btn = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
 }
+
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", function () {
+        console.log("Button clicked");
+        openModal();
+    });
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
